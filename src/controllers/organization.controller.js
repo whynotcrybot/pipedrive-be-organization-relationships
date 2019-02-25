525f3f83c8e24a0b40d0174eb62b81877c4b2e58
whynotcrybot <alexanderkatsero@gmail.com>
2019-02-25 14:28:22 +0200

    move traverse function to libs/

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-await-in-loop */
 const HTTPStatus = require('http-status');
+const traverseAndSave = require('../libs/traverseAndSave');
 
 async function getOrganization(req, res, next) {
   try {
@@ -14,56 +14,11 @@ async function getOrganization(req, res, next) {
   }
 }
 
-async function traverse(db, data) {
-  try {
-    const queue = [data];
-
-    await db.beginTransaction();
-
-    // eslint-disable-next-line
-    for (const organization of queue) {
-      // Insert current object
-      await db.query('INSERT IGNORE INTO organization SET name=?', organization.org_name);
-
-      // Traverse daughters
-      if (organization.daughters) {
-        const { daughters } = organization;
-        const promises = [];
-
-        daughters.forEach((daughter) => {
-          // Set sister relationships
-          const sisters = daughters.filter(sister => sister.org_name !== daughter.org_name);
-          sisters.forEach((sister) => {
-            promises.push(db.query(`INSERT INTO organizations_relationship (organization_id, linked_organization_id, relationship_type) VALUES ("${daughter.org_name}", "${sister.org_name}", "sister")`));
-          });
-
-          // Set parent and daughter relationship
-          promises.push(db.query(`INSERT INTO organizations_relationship (organization_id, linked_organization_id, relationship_type) VALUES ("${organization.org_name}", "${daughter.org_name}", "parent")`));
-          promises.push(db.query(`INSERT INTO organizations_relationship (organization_id, linked_organization_id, relationship_type) VALUES ("${daughter.org_name}", "${organization.org_name}", "daughter")`));
-
-          // Traverse daughter
-          queue.push(daughter);
-        });
-
-        await Promise.all(promises);
-      }
-    }
-
-    await db.commit();
-  } catch (e) {
-    await db.rollback();
-
-    return false;
-  }
-
-  return true;
-}
-
 async function createOrganization(req, res, next) {
   try {
     const { db, body } = req;
 
-    await traverse(db, body);
+    await traverseAndSave(db, body);
 
     const response = {
       success: true,
