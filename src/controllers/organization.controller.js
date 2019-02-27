333704d6006cd18b35263156e8455327b225433a
whynotcrybot <alexanderkatsero@gmail.com>
2019-02-27 13:15:57 +0200

    add pagination support

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -2,34 +2,41 @@ const HTTPStatus = require('http-status');
 const traverseAndSave = require('../libs/traverseAndSave');
 const generateAndSave = require('../libs/generateAndSave');
 
+const RESULTS_PER_PAGE = 100;
+
 async function getOrganization(req, res, next) {
   try {
     const { db, query } = req;
+    const { org, page } = query;
 
+    const validPage = !page || page <= 0 ? 1 : page;
+    const offset = RESULTS_PER_PAGE * (validPage - 1);
 
     const result = await db.query(`
       (
         SELECT DISTINCT organization_name AS org_name, 'parent' AS relationship_type
         FROM organizations_relationship
-        WHERE parent_organization_name="${query.org}"
+        WHERE parent_organization_name="${org}"
       )
       UNION
       (
         SELECT DISTINCT parent_organization_name AS org_name, 'daughter' AS relationship_type
         FROM organizations_relationship
-        WHERE organization_name="${query.org}"
+        WHERE organization_name="${org}"
       )
       UNION
       (
         SELECT DISTINCT parent_organization_name AS org_name, 'sister' as relationship_type
         FROM organizations_relationship
-        WHERE parent_organization_name!='${query.org}' AND organization_name IN (
+        WHERE parent_organization_name!='${org}' AND organization_name IN (
           SELECT organization_name
           FROM organizations_relationship
-          WHERE parent_organization_name='${query.org}'
+          WHERE parent_organization_name='${org}'
         )
       )
       ORDER BY org_name
+      LIMIT ${RESULTS_PER_PAGE}
+      OFFSET ${offset}
     `);
 
     return res.json(result);
